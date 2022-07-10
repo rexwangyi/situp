99732abe25ccee5d8d03355c03cf1db80fa9af29
wangyi <wy@rayfaydata.com>
2022-07-10 12:20:00 +0800

    update

--- js/main.js
+++ js/main.js
@@ -4,21 +4,46 @@ var leftTime = 0
 const countTime = 60
 var sportsID = 0
 var sportsStorage
+var sportsDB = TAFFY([])
+var curUser = "Other"
+const users = [
+  "William",
+  "Mom",
+  "Dad",
+  "Other"
+];
 
 
 $(function () {
-  sportsStorage = localStorage.getItem("sports")
-    ? JSON.parse(localStorage.getItem("sports"))
-    : []
+  // load user
+  curUser = localStorage.getItem("user") || "Other"
+  $("#pagetitle").text("Sit Up For " + curUser)
+  $("#user").val(curUser)
+  localStorage.setItem("user", curUser)
 
-  console.log(sportsStorage)
-  sportsDB = TAFFY(sportsStorage)
-  sportsID = sportsDB({ second: countTime }).order("situp desc").first().situp
-  if (sportsID === undefined) {
-    sportsID = 0
-  }
-  refresh()
+
+  // load DB
+  // sportsStorage = localStorage.getItem("sports")
+  //   ? JSON.parse(localStorage.getItem("sports"))
+  //   : []
+
+  // console.log(sportsStorage)
+  // sportsDB = TAFFY(sportsStorage)
+  // sportsID = sportsDB({ second: countTime }).order("situp desc").first().situp
+  // if (sportsID === undefined) {
+  //   sportsID = 0
+  // }
+
+  // refresh data
+  loadData(curUser)
 })
+
+$("#user").on('change', function () {
+  curUser = $(this).find(":selected").val();
+  $("#pagetitle").text("Sit Up For " + curUser)
+  localStorage.setItem("user", curUser)
+  loadData(curUser)
+});
 
 $(".circlebutton").on("click", function () {
   if (runStatus == 0) {
@@ -28,13 +53,13 @@ $(".circlebutton").on("click", function () {
     count = 0
     $("#count").text(0)
     $(".circlebutton").text(leftTime)
-    saveData(sportsID, leftTime, count, false)
+    saveData(curUser, sportsID, leftTime, count, false)
 
     var set = setInterval(function () {
       leftTime--
       $(".circlebutton").text(leftTime)
       if (leftTime === 0) {
-        saveData(sportsID, leftTime, count, true)
+        saveData(curUser, sportsID, leftTime, count, true)
         clearInterval(set)
         runStatus = 0
         leftTime = countTime
@@ -50,7 +75,7 @@ $(".circlebutton").on("click", function () {
         })
         sleep(5000).then(saveFinish);
       } else if (leftTime % 5 === 0) {
-        saveData(sportsID, leftTime, count, false)
+        saveData(curUser, sportsID, leftTime, count, false)
       }
     }, 1000)
   } else {
@@ -62,7 +87,28 @@ $(".circlebutton").on("click", function () {
   }
 })
 
-function saveData(id, nowLeftTime, nowCount, saveStrage) {
+function loadData(userID) {
+  var settings = {
+    "url": "https://getpantry.cloud/apiv1/pantry/39bfbe30-77b8-4315-a495-6dbef01d1607/basket/sports",
+    "method": "GET",
+    "timeout": 0,
+  };
+
+  $.ajax(settings).done(function (response) {
+    var sportsData = response[userID] || []
+    console.log(sportsData);
+
+    sportsDB = TAFFY(sportsData)
+    sportsID = sportsDB({ second: countTime }).order("situp desc").first().situp
+    if (sportsID === undefined) {
+      sportsID = 0
+    }
+    console.log("sportsID:"+sportsID);
+    refresh()
+  });
+}
+
+function saveData(userID, id, nowLeftTime, nowCount, saveDB) {
   var item = {
     situp: id,
     second: countTime - nowLeftTime,
@@ -72,9 +118,26 @@ function saveData(id, nowLeftTime, nowCount, saveStrage) {
   sportsDB.insert(item)
   console.log(item)
 
-  if (saveStrage) {
-    localStorage.setItem("sports", sportsDB().stringify());
-    console.log("save DB")
+  // if (saveDB) {
+  //   localStorage.setItem("sports", sportsDB().stringify());
+  //   console.log("save DB")
+  // }
+  if (saveDB) {
+    var settings = {
+      "url": "https://getpantry.cloud/apiv1/pantry/39bfbe30-77b8-4315-a495-6dbef01d1607/basket/sports",
+      "method": "PUT",
+      "timeout": 0,
+      "headers": {
+        "Content-Type": "application/json"
+      },
+      "data": JSON.stringify({
+        [userID]: sportsDB().stringify()
+      }),
+    };
+
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+    });
   }
 }
 
@@ -112,14 +175,14 @@ function refresh() {
               tension: 0.4
             }
           )
-        }else{
+        } else {
           chartDatset.push(
             {
-              label: 'Top'+(index + 1),
+              label: 'Top' + (index + 1),
               data: items,
               fill: false,
-              borderColor: NAMED_COLORS[index+2],
-              backgroundColor: NAMED_TRANS_COLORS[index+2],
+              borderColor: NAMED_COLORS[index + 2],
+              backgroundColor: NAMED_TRANS_COLORS[index + 2],
               tension: 0.4
             }
           )
